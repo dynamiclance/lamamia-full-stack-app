b10058cd2164e55614ba0e0c1be7b853d7025a89
sk <sajib.khan655@gmail.com>
2023-06-02 03:49:44 -0700

    contact page design done

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+
+import styles from "./page.module.css";
+import Button from '@/components/Button/Button';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            fill={true}
+            className={styles.img}
+            alt="contact us"
+          />
+        </div>
+        <div className={styles.formContainer}>
+          <input type='text' placeholder='Name' className={styles.input} />
+          <input type='text' placeholder='Email' className={styles.input} />
+          <textarea 
+            className={styles.textarea} 
+            placeholder='message'
+            cols="30" 
+            rows="10">
+            </textarea>
+            <Button url="#" text="Send"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
