4e0baeafd016c4390a4b62d87cb6bf672c8e92c1
sk <sajib.khan655@gmail.com>
2023-06-06 04:26:28 -0700

    dasboard register page design done

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
+
+import styles from "./page.module.css"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder='username' className={styles.input} required />
+        <input type="email" placeholder='email' className={styles.input} required />
+        <input type="password" placeholder='password' className={styles.input} required />
+        
+        <button className={styles.button}>Register</button>
+      </form>
+      <Link href="/dashboard/login">Login with exisiting account</Link>
+    </div>
   )
 }
 
-export default Register
+export default Register 
